472f98a8581fe9dca90c571327fb28348506c289
Gokul Murali <m.gokul595@gmail.com>
2018-03-17 15:30:53 +0530

    Revert "Revert "Changed test framework to `Rspec` and renamed `UsersController`""
    This reverts commit 4aa3841b2e31ec881494c903b71d972cbdaa11ca.

--- lib/rabbit_api/engine.rb
+++ lib/rabbit_api/engine.rb
@@ -1,5 +1,10 @@
 module RabbitApi
   class Engine < ::Rails::Engine
     isolate_namespace RabbitApi
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
