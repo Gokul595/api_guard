2dd467b9c145e403a75935b61d4fba1362a59e9b
Gokul Murali <m.gokul595@gmail.com>
2019-01-01 13:39:27 +0530

    Added simple coverage to rspec helper to check for coverage on running specs everytime.

--- spec/dummy/spec/spec_helper.rb
+++ spec/dummy/spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
