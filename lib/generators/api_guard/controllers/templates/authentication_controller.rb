c8947669cd6457fd883b8b387fef26f6cff35edf
Gokul Murali <m.gokul595@gmail.com>
2019-09-12 23:40:20 +0530

    Updated controller templates with translation logics.

--- lib/generators/api_guard/controllers/templates/authentication_controller.rb
+++ lib/generators/api_guard/controllers/templates/authentication_controller.rb
@@ -6,22 +6,22 @@ module <%= @controller_scope %>
     # def create
     #   if resource.authenticate(params[:password])
     #     create_token_and_set_header(resource, resource_name)
-    #     render_success(data: resource)
+    #     render_success(message: I18n.t('api_guard.authentication.signed_in'))
     #   else
-    #     render_error(422, message: 'Invalid login credentials')
+    #     render_error(422, message: I18n.t('api_guard.authentication.invalid_login_credentials'))
     #   end
     # end
 
     # def destroy
     #   blacklist_token
-    #   render_success(message: 'Signed out successfully')
+    #   render_success(message: I18n.t('api_guard.authentication.signed_out'))
     # end
 
     # private
 
     # def find_resource
     #   self.resource = resource_class.find_by(email: params[:email].downcase.strip) if params[:email].present?
-    #   render_error(422, message: 'Invalid login credentials') unless resource
+    #   render_error(422, message: I18n.t('api_guard.authentication.invalid_login_credentials')) unless resource
     # end
   end
 end
